9120e7a38cf7c4894200f809d9b2ac69b0ebbf17
khoujani <hosseinkhojany1380@gmail.com>
2022-08-27 18:57:32 +0430

    web

---
+++ web/main.dart.js_4.part.js
@@ -0,0 +1,20 @@
+self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
+$__dart_deferred_initializers__.current=function(a,b,c,$){var A={
+awc(){return new A.kI(null)},
+kI:function kI(d){this.a=d},
+Pp:function Pp(d){this.a=null
+this.b=d
+this.c=null}},C,B
+A=a.updateHolder(c[5],A)
+C=c[2]
+B=c[0]
+A.kI.prototype={
+a4(){return new A.Pp(C.l)}}
+A.Pp.prototype={
+F(d,e){var y=null
+return B.da(y,y,C.u,y,y,y,y,y,y,y,y,y,y)}}
+var z=a.updateTypes([]);(function inheritance(){var y=a.inherit
+y(A.kI,B.Y)
+y(A.Pp,B.a5)})()
+B.QE(b.typeUniverse,JSON.parse('{"kI":{"Y":[],"f":[]},"Pp":{"a5":["kI"]}}'))}
+$__dart_deferred_initializers__["Q9vaBw++30btN4EkLIo7Z1/yx0M="] = $__dart_deferred_initializers__.current
